da7e72115686def10eddf37d4cababc14c62a9f1
Jihad Omer <jhjahony@gmail.com>
2023-01-19 12:06:40 +0000

    use promises

--- callbacks.js
+++ callbacks.js
@@ -14,15 +14,30 @@ function getpost() {
   }, 1000);
 }
 
-function createNewPost(post, callback) {
-  setTimeout(() => {
-    posts.push(post);
-    callback();
-  }, 2000);
+/* 
+getpost(); add it as a parameter for createnewpost()
+why this function does not work ?
+because the DOM already paint the UI before the
+craete post function run it take longer time(2000), here the important of Async/Await came!!
+*/
+
+function createNewPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject(console.log("Somthing went Wrong"));
+      }
+    }, 500);
+  });
 }
 
-// getpost(); add it as a parameter for createnewpost()
-// why this function does not work ?
-// because the DOM already paint the UI before the
-// craete post function run it take longer time(2000), here the important of Async/Await came!!
-createNewPost({ title: "post three", body: "this is post three" }, getpost);
+createNewPost({ title: "post three", body: "this is Post Three" })
+.then(getpost)
+.catch(err => err)
+// .catch(err => console.log(err))
